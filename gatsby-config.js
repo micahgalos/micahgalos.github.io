8cb32a86bd2c040ea9bf9ea559ecfed04d6ccfbc
micahgalos <micahzugalos@gmail.com>
2022-06-06 17:17:10 -0700

    boot to master

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'ChandrikaDeb',
-        short_name: 'ChandrikaDeb',
+        name: 'MicahGalos',
+        short_name: 'MicahGalos',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
